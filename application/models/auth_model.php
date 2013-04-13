01c45ef0a27224f1dfd8a7cada876ee966b799f8
Mirco Macrelli <mmacrelli@gmail.com>
2013-04-13 17:46:44 +0200

    usa pbkdf2 in Auth_model->verify_user()

--- application/models/auth_model.php
+++ application/models/auth_model.php
@@ -4,6 +4,7 @@ class Auth_model extends CI_Model {
 	function __construct()
 	{
 		$this->load->database();
+        $this->load->helper('pbkdf2');
    }
 
    public function verify_user($email, $password)
@@ -13,14 +14,21 @@ class Auth_model extends CI_Model {
       $q = $this
             ->db
             ->where('email', $email)
-            ->where('password', sha1($password))
             ->limit(1)
             ->get('utenti');
 
-      if ( $q->num_rows > 0 ) {
-         // person has account with us
-         return $q->row();
-      }
-      return false;  // if($a === NULL)  php treats NULL, false, 0, and the empty string as equal.  (0_0)
+       // c'è un utente con questa password nel db?
+       if ($q->num_rows == 0) {
+           return false;
+       }
+
+       $user = $q->row();
+
+       // la password è corretta?
+       if (validate_password($password, $user->{'password'})) {
+           return $user;
+       } else {
+           return false;
+       }
    }
 }
